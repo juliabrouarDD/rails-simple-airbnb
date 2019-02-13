2835b838bb10d0c97898c6e608348d316acca52d
Julia Brouard <julia.brouard1@gmail.com>
2019-02-13 16:45:01 +0100

    exo 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  flat = Flat.new(
+    name: Faker::Company.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: Faker::Number.within(50..500),
+    number_of_guests: Faker::Number.within(1..10)
+  )
+  flat.save!
+end
